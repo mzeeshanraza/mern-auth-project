a1a1bb60b8b33ad2d4e3f3b12101759094e0a000
mzeeshanraza <mzeeshanraza77@gmail.com>
2025-04-17 11:55:09 +0500

    completed

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
